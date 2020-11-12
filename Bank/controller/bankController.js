7aa8e5cde03c7738a0fca21a0527e4e07de4ed4f
Tifferks <LP2296@gmail.com>
2020-11-12 21:50:08 +0100

    post merge

--- Bank/controller/bankController.js
+++ Bank/controller/bankController.js
@@ -13,19 +13,18 @@ let db = new sqlite3.Database(db_file, (err) => {
 export const postBankUser = async (req,res) => {
     let bankUserId = req.body.UserId
     const query = `INSERT INTO bank_user(UserId) VALUES(?)`
-    res.send('hello world')
 
-    // db.run(query, [bankUserId], (error) => {
-    //     if (error){
-    //         res.status(400).json({message: 'the user could not get crated, no rows affcted',
-    //         error : error.message})
-    //         console.log(error)
-    //     }
-    //     else{
-    //         console.log('new values were added to the database ')
-    //         res.status(201).json({message:'Rows were succesfully altered '})
-    //     }
-    // })
+    db.run(query, [bankUserId], (error) => {
+        if (error){
+            res.status(400).json({message: 'the user could not get crated, no rows affcted',
+            error : error.message})
+            console.log(error)
+        }
+        else{
+            console.log('new values were added to the database ')
+            res.status(201).json({message:'Rows were succesfully altered '})
+        }
+    })
 }
 
 export const testBankApi = async (req,res) => {
--- Borger/borger.js
+++ Borger/borger.js
@@ -1,29 +1,18 @@
 import express from 'express';
-<<<<<<< HEAD
-import cors from 'cors';
-
-import borgerRoutes from '../Borger/routes/routes.js';
-=======
 import sqlite3 from 'sqlite3'
 import axios from  'axios';
 import cors from 'cors';
 
 import borgerRoutes from './routes/routes.js'
->>>>>>> ab63a6df5ed7c4adaeb21ebef7d8b6a3ad7ff04d
 
 var app = express();
 
 app.use(express.json());
-<<<<<<< HEAD
-app.use(cors());
-app.use('/api/borger', borgerRoutes);
-=======
 app.use(cors())
 app.use('/borger',borgerRoutes)
 
 
 
->>>>>>> ab63a6df5ed7c4adaeb21ebef7d8b6a3ad7ff04d
 
 
 app.listen(5004, (err) => {
